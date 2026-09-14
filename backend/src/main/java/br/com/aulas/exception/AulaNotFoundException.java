c2f4b98934418d676410695dee7742fe4cbf53c4
Paola Ramos <paolasantosramos290@gmail.com>
2026-09-13 21:42:01 -0300

    Delete backend directory

--- backend/src/main/java/br/com/aulas/exception/AulaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.aulas.exception;
-
-public class AulaNotFoundException extends RuntimeException {
-
-    public AulaNotFoundException(Integer id) {
-        super("Aula não encontrada: " + id);
-    }
-}
--- backend/src/main/java/br/com/aulas/repository/AulaTemaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.aulas.repository;
-
-import br.com.aulas.model.AulaTema;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AulaTemaRepository extends JpaRepository<AulaTema, Integer> {
-}
